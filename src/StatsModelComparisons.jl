63395c19d02b01ef8771ad7e9868d25c57dc935e
Rob J Goedman <goedman@mac.com>
2021-02-27 13:30:46 -0700

    Rel 0.1.0 - After renaming to StatsModelComparisons.jl

---
+++ src/StatsModelComparisons.jl
@@ -0,0 +1,62 @@
+"""StatsModelComparisons
+
+This module implements several model comparison methods such as PSIS, WAIC and DIC..
+
+Included functions
+------------------
+psisloo
+    Pareto smoothed importance sampling leave-one-out log predictive densities.
+
+psislw
+    Pareto smoothed importance sampling.
+
+gpdfitnew
+    Estimate the paramaters for the Generalized Pareto Distribution (GPD).
+
+gpinv
+    Inverse Generalised Pareto distribution function.
+
+waic
+    ...
+
+dic
+    ...
+
+deviance
+    ...
+
+aic
+    Akaike Information Criterion
+
+bic 
+    Bayesian Information Criterion
+
+References
+----------
+Aki Vehtari, Andrew Gelman and Jonah Gabry (2015). Efficient implementation
+of leave-one-out cross-validation and WAIC for evaluating fitted Bayesian
+models. arXiv preprint arXiv:1507.04544.
+
+Aki Vehtari and Andrew Gelman (2015). Pareto smoothed importance sampling.
+arXiv preprint arXiv:1507.02646.
+"""
+
+module StatsModelComparisons
+
+    using StatsFuns, Statistics
+
+    mc_path = @__DIR__
+
+    include("psisloo.jl")
+    include("psislw.jl")
+    include("gpdfitnew.jl")
+    include("gpinv.jl")
+    include("waic.jl")
+    include("pk_utilities.jl")
+    include("dic.jl")
+    include("legacy.jl")
+
+    export mc_path, deviance, dic, psisloo, gpdfitnew, gpinv
+    export waic, aic, bic
+
+end
