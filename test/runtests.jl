bac87a69dab250d6444456416e9469e087ab698e
Rob J Goedman <goedman@mac.com>
2021-02-27 09:42:11 -0700

    Rel 0.1.0 - Initial tests with GitHub CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,11 @@
-using ModelComparisons
+using ModelComparisons, StanSample
 using Test
+
+if haskey(ENV, "JULIA_CMDSTAN_HOME")
+
+    ProjDir = @__DIR__
+    include(joinpath(ProjDir, "test_demo_wells.jl"))
+
+else
+  println("\nJULIA_CMDSTAN_HOME not set. Skipping tests")
+end
